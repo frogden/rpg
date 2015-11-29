0a131a627c449650af89402c11669933a1ea6015
frogden <idenis200077@gmail.com>
2015-11-29 15:38:25 +0300

    1.0.1

---
+++ rpg/Charecter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    public abstract class Charecter
+    {
+        public abstract string Skill1(List<Charecter> targets);
+        //public abstract string Skill2();
+        
+        private int hp;
+        public int Hp
+        {
+            get { return hp; }
+            set { hp = value; }
+        }
+        private int mp;
+        public int Mp
+        {
+            get { return mp; }
+            set { mp = value;}
+        }
+        private int power;
+        public int Power
+        {
+            get { return power; }
+            set { power = value; }
+        }
+        private int armor;
+        public int Armor
+        {
+            get { return armor; }
+            set { armor = value; }
+        }
+        private double evasion;
+        public virtual double Evasion
+        {
+            get { return evasion; }
+            set { evasion = value; }
+        }
+    }
+}
